da6651b3b72f38320dba6acb88f524b418bc7d6a
Charif <hifrook.zen5@gmail.com>
2025-07-02 21:14:56 +0700

    complete CRUD

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
@@ -65,12 +66,34 @@ def create_item(item: ItemCreate, db: Session = Depends(get_db)):
     return db_item  # Return the ORM model, which will be converted to Pydantic model by FastAPI
 
 
-# @app.get("/")
-# def hello_world():
-#     return {"message": "Hello, World!"}
+@app.get("/items/{item_id}", response_model=ItemResponse)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()  # Query the database for the item
+    return db_item
 
-# @app.get("/items", response_model=ItemResponse)
-# def read_item(db: Session = Depends(get_db)):
-#     list[ItemResponse]: items = get_db  # Call the function to get all items
-#     return  Session.query(Item).all()  # Get all items from the database
+@app.get("/items", response_model=list[ItemResponse])
+def read_item(db: Session = Depends(get_db)):
+    db_item = db.query(Item).all()  # Query the database for the item
+    return db_item
+
+@app.put("/items/{item_id}", response_model=ItemResponse)
+def update_item(item_id: int, item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first() 
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    for key, value in item.model_dump().items():
+        setattr(db_item, key, value) 
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db.delete(db_item)
+    db.commit()
+    return {"detail": "Item deleted successfully"}
+
 
