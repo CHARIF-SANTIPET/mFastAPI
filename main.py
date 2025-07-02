59e0dcc976a297a5bd4051ddfa5754665bbb5463
Charif <hifrook.zen5@gmail.com>
2025-07-02 15:46:32 +0700

    implement pydantic

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+# from typing import Union
+
+class Item(BaseModel):
+    name: str
+    price: float
 
 app = FastAPI()
 
-@app.get("/hello")
+@app.get("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items")
+def create_item(item: Item):
+    return {"requested_item": item}
